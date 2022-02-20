c779ceec770ee2a3ed1c2711e3d74448c9be6903
chethanmech <chethanmech.ncet@gmail.com>
2022-02-20 07:44:51 +0000

    chethan.java

--- chethan.java
+++ chethan.java
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 updating cm2_dev branch
+=======
+updaing the code cm2..
+updating the code cm1..
+>>>>>>> 71547d8... updating cm2_dev branch
 hey
 Iam updating the code in uat,
 Greetings of the day.
